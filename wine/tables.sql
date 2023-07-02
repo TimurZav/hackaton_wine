8fa7918a9eef841b99f328f05bfef49fa2320f4a
timur <timurka.zavyalov@mail.ru>
2023-07-02 15:06:58 +0300

    add: added new table

--- wine/tables.sql
+++ wine/tables.sql
@@ -1,5 +1,6 @@
-ATTACH TABLE _ UUID 'ab4ebf8a-55a0-4741-b69a-68f096e0957c'
+CREATE TABLE default.wine_map
 (
+
     `id` String,
     `coordinate_x1` Nullable(Decimal(38, 19)),
     `coordinate_y1` Nullable(Decimal(38, 19)),
@@ -25,4 +26,24 @@ ATTACH TABLE _ UUID 'ab4ebf8a-55a0-4741-b69a-68f096e0957c'
 )
 ENGINE = MergeTree
 ORDER BY id
-SETTINGS index_granularity = 8192
+
+
+
+CREATE TABLE default.wine
+(
+    `fixed_acidity` Decimal(38, 19),
+    `volatile_acidity` Nullable(Decimal(38, 19)),
+    `citric_acid` Nullable(Decimal(38, 19)),
+    `residual_sugar` Nullable(Decimal(38, 19)),
+    `chlorides` Nullable(Decimal(38, 19)),
+    `free_sulfur_dioxide` Nullable(Decimal(38, 19)),
+    `total_sulfur_dioxide` Nullable(Decimal(38, 19)),
+    `density` Nullable(Decimal(38, 19)),
+    `pH` Nullable(Decimal(38, 19)),
+    `sulphates` Nullable(Decimal(38, 19)),
+    `alcohol` Nullable(Decimal(38, 19)),
+    `winecolor` Nullable(String),
+    `quality` Nullable(Decimal(38, 19))
+)
+ENGINE = MergeTree
+ORDER BY fixed_acidity
